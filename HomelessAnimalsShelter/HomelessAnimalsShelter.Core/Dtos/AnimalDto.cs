7fe256bc6dfa6f0e398156310e2550d1c8e85054
coolestProcaviidae <85066178+genericWindowsUser416@users.noreply.github.com>
2025-02-15 19:52:24 +0300

    Create AnimalDto.cs

---
+++ HomelessAnimalsShelter/HomelessAnimalsShelter.Core/Dtos/AnimalDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Zooparchik.Core.Dtos
+{
+	public class AnimalDto
+	{
+		public int? Id { get; set; }
+
+		public string Name { get; set; }
+
+		public string Description { get; set; }
+
+		public int? Number { get; set; }
+	}
+}
