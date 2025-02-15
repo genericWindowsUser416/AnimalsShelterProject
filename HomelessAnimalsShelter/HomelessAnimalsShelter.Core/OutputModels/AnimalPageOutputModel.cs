45eb915fc7da2b3fb36ae0b78f117c3a840f011a
WhiteoutHHS <whiteouthhs@gmail.com>
2025-02-15 20:25:49 +0300

    Update

--- HomelessAnimalsShelter/HomelessAnimalsShelter.Core/OutputModels/AnimalPageOutputModel.cs
+++ HomelessAnimalsShelter/HomelessAnimalsShelter.Core/OutputModels/AnimalPageOutputModel.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace HomelessAnimalsShelter.Core.OutputModels
 {
-    internal class AnimalPageOutputModel
+    public class AnimalOutputModel
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public List<string> TagList { get; set; } = new List<string>();
     }
 }
+
