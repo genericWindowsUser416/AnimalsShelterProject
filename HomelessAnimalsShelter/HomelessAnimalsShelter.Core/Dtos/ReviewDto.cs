80d2a223c03579b715451764510acc7368691106
Nik <oknik2009@gmail.com>
2025-02-16 13:08:34 +0300

    ReviewDto.cs

---
+++ HomelessAnimalsShelter/HomelessAnimalsShelter.Core/Dtos/ReviewDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomelessAnimalsShelter.Core.Dtos
+{
+	internal class ReviewDto
+	{
+		public int Id { get; set; }
+
+		public int Buyer_Id { get; set; }
+
+		public int Shelter_Id { get; set; }
+
+		public string Description { get; set; }
+
+		public int Rating { get; set; }
+	}
+}
