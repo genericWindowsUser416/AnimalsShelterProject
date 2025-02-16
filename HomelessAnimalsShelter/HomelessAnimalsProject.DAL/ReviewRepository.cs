8f8679b68d428fd35791838d1a4245d495129776
coolestProcaviidae <85066178+genericWindowsUser416@users.noreply.github.com>
2025-02-16 20:20:21 +0300

    Create ReviewRepository.cs

---
+++ HomelessAnimalsShelter/HomelessAnimalsProject.DAL/ReviewRepository.cs
@@ -0,0 +1,51 @@
+﻿using HomelessAnimalsShelter.Core.Dtos;
+
+namespace HomelessAnimalsShelter.DAL
+{
+	public class ReviewRepository
+	{
+		public List<ReviewDto> GetAllReviews()
+		{
+			var r1 = new ReviewDto()
+			{
+				Buyer_Id = 1,
+				Shelter_Id = 1,
+				Id = 1,
+				Rating = 5,
+				Description = "Всё супер"
+			};
+
+			var r2 = new ReviewDto()
+			{
+				Buyer_Id = 2,
+				Shelter_Id = 1,
+				Id = 2,
+				Rating = 3,
+				Description = "Нормально"
+			};
+
+			var r3 = new ReviewDto()
+			{
+				Buyer_Id = 3,
+				Shelter_Id = 2,
+				Id = 3,
+				Rating = 2,
+				Description = "QQQ"
+			};
+
+			return new List<ReviewDto> { r1, r2, r3 };
+		}
+
+		public ReviewDto GetReviewById(int id)
+		{
+			return new ReviewDto()
+			{
+				Buyer_Id = 3,
+				Shelter_Id = 5,
+				Id = 4,
+				Rating = 4,
+				Description = "WWW"
+			};
+		}
+	}
+}
