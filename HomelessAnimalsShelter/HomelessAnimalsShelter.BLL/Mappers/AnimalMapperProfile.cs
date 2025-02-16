8154895efd02eeb960b93f7ba1aec157597e875b
coolestProcaviidae <85066178+genericWindowsUser416@users.noreply.github.com>
2025-02-16 21:12:59 +0300

    created a Core project reference to BLL

--- HomelessAnimalsShelter/HomelessAnimalsShelter.BLL/Mappers/AnimalMapperProfile.cs
+++ HomelessAnimalsShelter/HomelessAnimalsShelter.BLL/Mappers/AnimalMapperProfile.cs
@@ -1,4 +1,7 @@
 ﻿using AutoMapper;
+using HomelessAnimalsShelter.Core.Dtos;
+using HomelessAnimalsShelter.Core.Models;
+
 namespace HomelessAnimalsShelter.BLL.Mappers
 {
 	public class AnimalMapperProfile:Profile
