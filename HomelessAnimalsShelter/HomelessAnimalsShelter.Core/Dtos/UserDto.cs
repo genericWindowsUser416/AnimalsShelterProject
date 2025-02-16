5e03467fc0c4f87fa685f783db3d6bf850dfd144
coolestProcaviidae <85066178+genericWindowsUser416@users.noreply.github.com>
2025-02-16 09:41:33 +0300

    created Core/Dtos/UserDto.cs

---
+++ HomelessAnimalsShelter/HomelessAnimalsShelter.Core/Dtos/UserDto.cs
@@ -0,0 +1,13 @@
+﻿namespace HomelessAnimalsShelter.Core.Dtos
+{
+	public class UserDto
+	{
+		public int? Id { get; set; }
+
+		public string Login { get; set; }
+
+		public string? Password { get; set; }
+
+		public int? UserRole { get; set; }
+	}
+}
