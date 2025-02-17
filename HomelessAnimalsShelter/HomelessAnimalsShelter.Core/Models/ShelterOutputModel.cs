63016c3db676406403d58e44ee8705fcdd05d782
coolestProcaviidae <85066178+genericWindowsUser416@users.noreply.github.com>
2025-02-17 20:57:41 +0300

    Create ShelterOutputModel.cs

---
+++ HomelessAnimalsShelter/HomelessAnimalsShelter.Core/Models/ShelterOutputModel.cs
@@ -0,0 +1,15 @@
+﻿namespace HomelessAnimalsShelter.Core.Models
+{
+	public class ShelterOutputModel
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public string Description { get; set; }
+
+		public string City { get; set; }
+
+		public List<int> AnimalList { get; set; } = new List<int>();
+	}
+}
