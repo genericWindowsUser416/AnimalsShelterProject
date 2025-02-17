847b890d03a08d3e21d41cddd11d1be4e9253acc
coolestProcaviidae <85066178+genericWindowsUser416@users.noreply.github.com>
2025-02-17 19:43:42 +0300

    created AnimalManager.cs and fixed dependencies

---
+++ HomelessAnimalsShelter/HomelessAnimalsShelter.BLL/AnimalManager.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using HomelessAnimalsShelter.Core.Models;
+using HomelessAnimalsShelter.BLL.Mappers;
+using HomelessAnimalsShelter.DAL;
+namespace HomelessAnimalsShelter.BLL
+{
+    public class AnimalManager
+    {
+		private AnimalRepository _repository;
+		private Mapper _mapper;
+
+		public AnimalManager()
+		{
+			MapperConfiguration configuration = new MapperConfiguration(
+				cfg => {
+					cfg.AddProfile(new AnimalMapperProfile());
+				});
+
+			_mapper = new Mapper(configuration);
+
+			_repository = new AnimalRepository();
+		}
+
+		public List<AnimalCardOutputModel> GetAllAnimals()
+		{
+			return _mapper.Map<List<AnimalCardOutputModel>>(_repository.GetAllAnimals());
+		}
+
+		public AnimalPageOutputModel GetAnimalById(int id)
+		{
+			return _mapper.Map<AnimalPageOutputModel>(_repository.GetAnimalById(id));
+		}
+	}
+}
