88bb0268c132431827ce1d0334cd39acd974ef0f
coolestProcaviidae <85066178+genericWindowsUser416@users.noreply.github.com>
2025-02-17 20:21:05 +0300

    made Review MapperProfile + OutputModel
    also fixed AnimalDto

---
+++ HomelessAnimalsShelter/HomelessAnimalsShelter.BLL/Mappers/ReviewMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using HomelessAnimalsShelter.Core.Dtos;
+using HomelessAnimalsShelter.Core.Models;
+
+namespace HomelessAnimalsShelter.BLL.Mappers
+{
+	public class ReviewMapperProfile : Profile
+	{
+		public ReviewMapperProfile()
+		{
+			CreateMap<ReviewDto, ReviewOutputModel>();
+		}
+	}
+}
---
+++ HomelessAnimalsShelter/HomelessAnimalsShelter.Core/Models/ReviewOutputModel.cs
@@ -0,0 +1,15 @@
+﻿namespace HomelessAnimalsShelter.Core.Models
+{
+    public class ReviewOutputModel
+    {
+		public int Id { get; set; }
+
+		public int Buyer_Id { get; set; }
+
+		public int Shelter_Id { get; set; }
+
+		public string Description { get; set; }
+
+		public int Rating { get; set; }
+	}
+}
