fa0bd31b5b9db99b1fa6ea47bde7d53451cee9f9
coolestProcaviidae <85066178+genericWindowsUser416@users.noreply.github.com>
2025-02-17 16:44:32 +0300

    fixed ShelterDto.cs and made ShelterRepository.cs

---
+++ HomelessAnimalsShelter/HomelessAnimalsProject.DAL/ShelterRepository.cs
@@ -0,0 +1,55 @@
+﻿using HomelessAnimalsShelter.Core.Dtos;
+
+namespace HomelessAnimalsShelter.DAL
+{
+	public class ShelterRepository
+	{
+		public List<ShelterDto> GetAllShelters()
+		{
+			var s1 = new ShelterDto()
+			{
+				Id = 1,
+				Buyer_Id = 1,
+				Name = "Прют QQQ",
+				Description = "лучший приют 2025",
+				City = "Омск",
+				AnimalList = { 1, 2, 3, 4 },
+			};
+
+			var s2 = new ShelterDto()
+			{
+				Id = 2,
+				Buyer_Id = 2,
+				Name = "Прют WWW",
+				Description = "лучший приют 2026",
+				City = "Оренбург",
+				AnimalList = { 5, 6, 7, 8 },
+			};
+
+			var s3 = new ShelterDto()
+			{
+				Id = 3,
+				Buyer_Id = 3,
+				Name = "Приют ещё один",
+				Description = "лучший приют 2301",
+				City = "Москва",
+				AnimalList = { 9, 10, 11, 12 },
+			};
+
+			return new List<ShelterDto> { s1, s2, s3 };
+		}
+
+		public ShelterDto GetShelterById(int id)
+		{
+			return new ShelterDto()
+			{
+				Id = id,
+				Buyer_Id = 5,
+				Name = "Приют такой-то",
+				Description = "лучший приют всегда",
+				City = "Там",
+				AnimalList = { 13, 14, 15 },
+			};
+		}
+	}
+}
--- HomelessAnimalsShelter/HomelessAnimalsShelter.Core/Dtos/ShelterDto.cs
+++ HomelessAnimalsShelter/HomelessAnimalsShelter.Core/Dtos/ShelterDto.cs
@@ -2,7 +2,7 @@
 {
 	public class ShelterDto
 	{
-		public string Id { get; set; }
+		public int Id { get; set; }
 
 		public string Name { get; set; }
 
