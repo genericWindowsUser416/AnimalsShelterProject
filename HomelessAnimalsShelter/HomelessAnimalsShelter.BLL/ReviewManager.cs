1026c925f0481d0bef93102ec15219536c0c5d9b
Nik <oknik2009@gmail.com>
2025-02-18 19:28:25 +0300

    ReviewManager.cs

---
+++ HomelessAnimalsShelter/HomelessAnimalsShelter.BLL/ReviewManager.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using HomelessAnimalsShelter.BLL.Mappers;
+using HomelessAnimalsShelter.Core.Models;
+using HomelessAnimalsShelter.DAL;
+
+namespace HomelessAnimalsShelter.BLL
+{
+	internal class ReviewManager
+	{
+		private ReviewRepository _repository;
+		private Mapper _mapper;
+
+		public ReviewManager()
+		{
+			MapperConfiguration configuration = new MapperConfiguration(
+				cfg => {
+					cfg.AddProfile(new ReviewMapperProfile());
+				});
+
+			_mapper = new Mapper(configuration);
+
+			_repository = new ReviewRepository();
+		}
+
+		public List<AnimalCardOutputModel> GetAllReviews()
+		{
+			return _mapper.Map<List<AnimalCardOutputModel>>(_repository.GetAllReviews());
+		}
+
+		public AnimalPageOutputModel GetReviewsById(int id)
+		{
+			return _mapper.Map<AnimalPageOutputModel>(_repository.GetReviewById(id));
+		}
+	}
+}
