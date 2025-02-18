ee30ed6e31da9a9e4bec6667e14d5d92dba229d7
coolestProcaviidae <85066178+genericWindowsUser416@users.noreply.github.com>
2025-02-18 18:06:29 +0300

    created DonationOutputModel, DonationMapperProfile

---
+++ HomelessAnimalsShelter/HomelessAnimalsShelter.Core/Models/DonationOutputModel.cs
@@ -0,0 +1,17 @@
+﻿namespace HomelessAnimalsShelter.Core.Models
+{
+    public class DonationOutputModel
+    {
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public string Description { get; set; }
+
+		public int Shelter_Id { get; set; }
+
+		public Decimal NeededMoney { get; set; }
+
+		public Decimal CollectedMoney { get; set; }
+	}
+}
