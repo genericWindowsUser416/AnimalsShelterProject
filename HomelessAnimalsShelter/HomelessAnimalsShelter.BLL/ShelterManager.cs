367e83a03bdbecb930a4f6a9e76e71f17f9a833e
coolestProcaviidae <85066178+genericWindowsUser416@users.noreply.github.com>
2025-02-18 19:27:01 +0300

    Create ShelterManager.cs

---
+++ HomelessAnimalsShelter/HomelessAnimalsShelter.BLL/ShelterManager.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using HomelessAnimalsShelter.Core.Models;
+using HomelessAnimalsShelter.BLL.Mappers;
+using HomelessAnimalsShelter.DAL;
+
+namespace HomelessAnimalsShelter.BLL
+{
+	public class ShelterManager
+	{
+		private ShelterRepository _repository;
+		private Mapper _mapper;
+
+		public ShelterManager()
+		{
+			MapperConfiguration configuration = new MapperConfiguration(
+				cfg => {
+					cfg.AddProfile(new ShelterMapperProfile());
+				});
+
+			_mapper = new Mapper(configuration);
+
+			_repository = new ShelterRepository();
+		}
+
+		public ShelterOutputModel GetShelterById(int id)
+		{
+			return _mapper.Map<ShelterOutputModel>(_repository.GetShelterById(id));
+		}
+	}
+}
