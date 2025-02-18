89adc5586c8921e46cebbea075a3bb5ff734da08
coolestProcaviidae <85066178+genericWindowsUser416@users.noreply.github.com>
2025-02-18 19:22:37 +0300

    Create DonationManager.cs

---
+++ HomelessAnimalsShelter/HomelessAnimalsShelter.BLL/DonationManager.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using HomelessAnimalsShelter.Core.Models;
+using HomelessAnimalsShelter.BLL.Mappers;
+using HomelessAnimalsShelter.DAL;
+
+namespace HomelessAnimalsShelter.BLL
+{
+	public class DonationManager
+	{
+		private DonationRepository _repository;
+		private Mapper _mapper;
+
+		public DonationManager()
+		{
+			MapperConfiguration configuration = new MapperConfiguration(
+				cfg => {
+					cfg.AddProfile(new DonationMapperProfile());
+				});
+
+			_mapper = new Mapper(configuration);
+
+			_repository = new DonationRepository();
+		}
+
+		public DonationOutputModel GetDonationById(int id)
+		{
+			return _mapper.Map<DonationOutputModel>(_repository.GetDonationById(id));
+		}
+	}
+}
