d30468c0c7a4970ddc44e6a7af506ea3cf48e11e
coolestProcaviidae <85066178+genericWindowsUser416@users.noreply.github.com>
2025-02-22 13:05:18 +0300

    updated AnimalRepostiory, ShelterCardTopPartBuyer

--- HomelessAnimalsShelter/HomelessAnimalsProject.DAL/AnimalRepository.cs
+++ HomelessAnimalsShelter/HomelessAnimalsProject.DAL/AnimalRepository.cs
@@ -4,40 +4,28 @@ namespace HomelessAnimalsShelter.DAL
 {
 	public class AnimalRepository
 	{
-		public List<AnimalDto> GetAllAnimals()
-		{
-			var a1 = new AnimalDto()
-			{
-				Id = 1,
-				Name = "Кот",
-				Description = "Спит"
-			};
+        public AnimalDto GetAnimalById(int id)
+        {
+            using (Context context = new Context())
+            {
+                return context.Animals.Where(a => a.Id == id).First();
+            }
+        }
 
-			var a2 = new AnimalDto()
-			{
-				Id = 2,
-				Name = "Собака",
-				Description = "Быстро бегает",
-			};
-
-			var a3 = new AnimalDto()
-			{
-				Id = 3,
-				Name = "Морская свинка",
-				Description = "Спит"
-			};
-
-			return new List<AnimalDto> { a1, a2, a3 };
-		}
-
-		public AnimalDto GetAnimalById(int id)
-		{
-			return new AnimalDto()
-			{
-				Id = id,
-				Name = "Глеб",
-				Description = "олег"
-			};
-		}
-	}
+        public List<AnimalDto> GetAllAnimals()
+        {
+            using (Context context = new Context())
+            {
+                return context.Animals
+                              .Select(a => new AnimalDto
+                              {
+                                  Id = a.Id,
+                                  Name = a.Name,
+                                  Description = a.Description,
+                                  TagDtos = a.TagDtos
+                              })
+                              .ToList();
+            }
+        }
+    }
 }
