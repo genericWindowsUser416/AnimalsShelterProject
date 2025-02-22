e2b18867e9dc064af6dfff7f45a54253665caf61
coolestProcaviidae <85066178+genericWindowsUser416@users.noreply.github.com>
2025-02-22 12:24:54 +0300

    Update ShelterRepository.cs

--- HomelessAnimalsShelter/HomelessAnimalsProject.DAL/ShelterRepository.cs
+++ HomelessAnimalsShelter/HomelessAnimalsProject.DAL/ShelterRepository.cs
@@ -4,44 +4,31 @@ namespace HomelessAnimalsShelter.DAL
 {
 	public class ShelterRepository
 	{
-		public List<ShelterDto> GetAllShelters()
-		{
-			var s1 = new ShelterDto()
-			{
-				Id = 1,
-				Name = "Прют QQQ",
-				Description = "лучший приют 2025",
-				City = "Омск"
-			};
-
-			var s2 = new ShelterDto()
-			{
-				Id = 2,
-				Name = "Прют WWW",
-				Description = "лучший приют 2026",
-				City = "Оренбург"
-			};
-
-			var s3 = new ShelterDto()
-			{
-				Id = 3,
-				Name = "Приют ещё один",
-				Description = "лучший приют 2301",
-				City = "Москва"
-			};
-
-			return new List<ShelterDto> { s1, s2, s3 };
-		}
-
 		public ShelterDto GetShelterById(int id)
 		{
-			return new ShelterDto()
+			using (Context context = new Context())
 			{
-				Id = id,
-				Name = "Приют такой-то",
-				Description = "лучший приют всегда",
-				City = "Там"
-			};
+				return context.Shelters.Where(s => s.Id == id).First();
+			}
+		}
+
+		public List<ShelterDto> GetAllShelters()
+		{
+			using (Context context = new Context())
+			{
+				return context.Shelters
+							  .Select(s => new ShelterDto
+							  {
+								  Id = s.Id,
+								  Name = s.Name,
+								  Description = s.Description,
+								  City = s.City,
+								  DonationDtos = s.DonationDtos,
+								  User = s.User,
+								  AnimalDtos = s.AnimalDtos
+							  })
+							  .ToList();
+			}
 		}
 	}
 }
