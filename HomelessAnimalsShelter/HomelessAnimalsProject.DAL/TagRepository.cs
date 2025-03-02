f528e5f452223b9490dc6cdcf5a58e931abbfe41
coolestProcaviidae <85066178+genericWindowsUser416@users.noreply.github.com>
2025-03-02 12:15:44 +0300

    Create TagRepository.cs

---
+++ HomelessAnimalsShelter/HomelessAnimalsProject.DAL/TagRepository.cs
@@ -0,0 +1,29 @@
+﻿using HomelessAnimalsShelter.Core.Dtos;
+
+namespace HomelessAnimalsShelter.DAL
+{
+    public class TagRepository
+    {
+        public TagDto GetTagById(int id)
+        {
+            using (Context context = new Context())
+            {
+                return context.Tags.Where(t => t.Id == id).First();
+            }
+        }
+
+        public List<TagDto> GetAllTags()
+        {
+            using (Context context = new Context())
+            {
+                return context.Tags
+                              .Select(t => new TagDto
+                              {
+                                  Id = t.Id,
+                                  Name = t.Name
+                              })
+                              .ToList();
+            }
+        }
+    }
+}
