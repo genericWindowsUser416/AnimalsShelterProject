2aefff7b12516fb877df0fcf46ce20203b65a61f
coolestProcaviidae <85066178+genericWindowsUser416@users.noreply.github.com>
2025-03-02 12:20:42 +0300

    created TagMapperProfile and TagManager

---
+++ HomelessAnimalsShelter/HomelessAnimalsShelter.BLL/Mappers/TagMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using HomelessAnimalsShelter.Core.Dtos;
+using HomelessAnimalsShelter.Core.Models;
+
+namespace HomelessAnimalsShelter.BLL.Mappers
+{
+    public class TagMapperProfile : Profile
+    {
+        public TagMapperProfile()
+        {
+            CreateMap<TagDto, TagOutputModel>();
+        }
+    }
+}
---
+++ HomelessAnimalsShelter/HomelessAnimalsShelter.BLL/TagManager.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using HomelessAnimalsShelter.Core.Models;
+using HomelessAnimalsShelter.BLL.Mappers;
+using HomelessAnimalsShelter.DAL;
+
+namespace HomelessAnimalsShelter.BLL
+{
+    public class TagManager
+    {
+        private TagRepository _repository;
+        private Mapper _mapper;
+
+        public TagManager()
+        {
+            MapperConfiguration configuration = new MapperConfiguration(
+                cfg => {
+                    cfg.AddProfile(new TagMapperProfile());
+                });
+
+            _mapper = new Mapper(configuration);
+
+            _repository = new TagRepository();
+        }
+
+        public List<TagOutputModel> GetAllTags()
+        {
+            return _mapper.Map<List<TagOutputModel>>(_repository.GetAllTags());
+            //return null;
+        }
+
+        public TagOutputModel GetTagById(int id)
+        {
+            return _mapper.Map<TagOutputModel>(_repository.GetTagById(id));
+        }
+    }
+}
